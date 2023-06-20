6f88c70c5a5fdd8503b7fbd267b97577423409ec
anaisberg <anais.schlienger@lilo.org>
2023-06-20 15:51:06 +0200

    ✨ sort stacks when listing or deleting

--- functions/listStacks/handler.ts
+++ functions/listStacks/handler.ts
@@ -1,10 +1,11 @@
 import omit from 'lodash/omit';
 
 import { listStacksInputSchema } from '@functions/listStacks/input.schema';
+import { Project } from '@libs/entities/project';
 import { Stack } from '@libs/entities/stack';
 import { applyHttpMiddleware } from '@libs/middlewares';
 import type { CustomAPIGatewayProxyHandler } from '@libs/utils';
-import { success, throwIfNil } from '@libs/utils';
+import { sortStacks, success, throwIfNil } from '@libs/utils';
 import { projectKeyAuthorizer } from '@libs/utils/http/projectKeyAuthorizer';
 
 const listStacks: CustomAPIGatewayProxyHandler<typeof listStacksInputSchema, unknown> = async ({
@@ -13,10 +14,14 @@ const listStacks: CustomAPIGatewayProxyHandler<typeof listStacksInputSchema, unk
   await projectKeyAuthorizer(projectKey);
 
   const { Items: stacks } = await Stack.query(Stack.name, { beginsWith: projectKey });
+  const { Item: project } = await Project.get({ projectKey });
 
   throwIfNil(stacks, 'stacks must exist');
+  const sortedStacks = sortStacks(stacks, project?.prefix);
 
-  return success({ stacks: stacks.map(stack => omit(stack, 'projectKey')) });
+  return success({
+    stacks: sortedStacks.map(stack => omit(stack, 'projectKey')),
+  });
 };
 
 export const main = applyHttpMiddleware(listStacks, { inputSchema: listStacksInputSchema });
--- libs/utils/index.ts
+++ libs/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './http';
 export * from './env';
 export * from './enums';
+export * from './sortStacks';
 export * from './isLocalLambdaInvocation';
 export * from './typeGuards';
 export * from './constants';
